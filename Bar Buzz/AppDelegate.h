9ccb74ea1cc5b1918966c3c24515dc92a6dfd9c2
Owen Yang <yoyoyango123@gmail.com>
2015-03-06 14:41:53 -0500

    Initial Commit

---
+++ Bar Buzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bar Buzz
+//
+//  Created by Owen Yang on 3/6/15.
+//  Copyright (c) 2015 Bar Buzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
